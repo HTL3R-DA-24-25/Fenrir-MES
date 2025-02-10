2062bb9ffc1e76bb0fb17081794296f186ab49ea
LeyLao <bastian.uhlig@gmx.at>
2025-02-10 11:37:49 +0100

    Cookie handling in API

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,22 @@
+import { getAllDataPoints, loginUser } from "@/pages/scada-handler";
+
+async function handleLogin() {
+  try {
+    await loginUser("admin", "admin");
+    const data = await getAllDataPoints();
+    // console.log("Data points:", data);
+  } catch (error) {
+    console.error("Error logging in:", error);
+  }
+}
+
 export default function Dashboard() {
-    return (
-        <div className={`flex items-center justify-center min-h-screen dark:bg-gray-900 bg-gray-100`}>
-            <div>DASHBOARD</div>
-        </div>
-    );
+  handleLogin();
+  return (
+    <div
+      className={`flex items-center justify-center min-h-screen dark:bg-gray-900 bg-gray-100`}
+    >
+      <div>DASHBOARD</div>
+    </div>
+  );
 }
